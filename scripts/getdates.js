a4ea120099482a72cd0cb18439be4867d8814751
Jayweek4246c <geraldtang669@gmail.com>
2025-05-08 16:26:01 +0300

    Home Page creation

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,12 @@
+// Select DOM elements for output
+const currentyear = document.querySelector("#currentyear");
+const lastmodification = document.querySelector("#last-Modified");
 
+//Create date object
+const today = new Date();
+const lastmod = new Date(document.lastModified);
+
+//output to html
+currentyear.innerHTML = `${today.getFullYear()}`;
+
+lastmodification.innerHTML = `Last modification: ${lastmod}`;
